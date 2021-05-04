b181d7a3719610d1cb8122bcd2b10548625a3eef
Skylar Kim <kimsooye@usc.edu>
2021-05-04 18:05:41 -0400

    update config omfg

--- config/config.php
+++ config/config.php
@@ -3,12 +3,12 @@ session_start(); // put it here so that session is started on every page that re
 
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$server = $url["host"];
+$dbhost = $url["host"];
 $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"],1);
 
-define('DB_HOST', $host);
+define('DB_HOST', $dbhost);
 define('DB_USER', $username);
 define('DB_PASS', $password);
 define('DB_NAME', $db);
